05c11185ef4387ce9fafe1a11c789346ffa97085
richietam <richardtam.ny@gmail.com>
2019-03-24 21:46:52 -0400

    finished lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only:[:index, :show]
+  root to: 'application#home'
+
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  post '/logout', to: "sessions#destroy"
+
 end
